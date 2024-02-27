590a2a8e8fceab8cd6a1bdacbc1f56e12d28d2e6
Paul <abengaristide@gmail.com>
2024-02-27 15:51:24 +0100

    retouche page acceuil

--- src/app/page-acceuil-sans-compte/page-acceuil-sans-compte.component.ts
+++ src/app/page-acceuil-sans-compte/page-acceuil-sans-compte.component.ts
@@ -11,6 +11,13 @@ export class PageAcceuilSansCompteComponent {
   isMenuIconClosed = true;
   showSearchBar = false;
 
+  description_Metier:string = "Découvre l'univers des métiers  avec  LABOUSSOLE ! Explorez une multitude de professions passionnantes et trouvez le métier qui correspond à ton profil."
+  description_Bourses:string = "LABOUSSOLE te donne accès à un large éventail de bourses d'études pour t’aider à financer tes études et réaliser tes rêves"
+  description_Voyages_Etudes:string = "Tu as envie de poursuivre tes études à l’extérieur ?  Nous mettons à ta disposition les informations dont tu auras besoins pour réussir à l’étranger."
+  description_Formation:string = "LABOUSSOLE te donne les informations sur toutes les offres de formations au Cameroun et au Congo afin que tu puisses choisir aisément ta Filière d’étude."
+  
+
+
   toggleMenu() {
     this.isMenuIconClicked = !this.isMenuIconClicked;
     this.isMenuIconClosed = !this.isMenuIconClosed;
