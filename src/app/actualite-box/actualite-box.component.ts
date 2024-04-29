1302e9f7730ace3b4e8667557f63bb3f98dbe7ff
Paul <abengaristide@gmail.com>
2024-04-29 11:03:39 +0100

    latest changes

--- src/app/actualite-box/actualite-box.component.ts
+++ src/app/actualite-box/actualite-box.component.ts
@@ -11,4 +11,44 @@ export class ActualiteBoxComponent {
 @Input() title:string = ""
 @Input() description:string = ""
 @Input() tags:string = ""
+
+
+constructor(){
+
+}
+
+ngOnInit(){
+  let image= this.convertDriveLinkToDirectDownloadLink(this.image)?this.convertDriveLinkToDirectDownloadLink(this.image):"";
+  if(image){
+    this.image=image;
+  }
+}
+convertDriveLinkToDirectDownloadLink(driveLink: string): string | null {
+  if(driveLink){
+    const regexDirectDownload = /https:\/\/drive\.google\.com\/thumbnail\?&id=([^/]+)/;
+    const matchDirectDownload = driveLink.match(regexDirectDownload);
+
+    if (matchDirectDownload && matchDirectDownload.length === 2) {
+        // The link is already in the desired format, return it as is
+        return driveLink;
+    }
+
+    // Check if the provided link matches the Google Drive file link pattern
+    const regexFileLink = /https:\/\/drive\.google\.com\/file\/d\/([^/]+)\/view\?usp=sharing/;
+    const matchFileLink = driveLink.match(regexFileLink);
+
+    if (matchFileLink && matchFileLink.length === 2) {
+        const fileId = matchFileLink[1];
+        const directDownloadLink = `https://drive.google.com/thumbnail?&id=${fileId}`;
+        return directDownloadLink;
+    } else {
+        console.error("Invalid Google Drive shareable link format.");
+        return null;
+    }
+  }else{
+    return null;
+  }
+}
+
+
 }
